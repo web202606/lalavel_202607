459dd4fd1ab58c71e4390b865c77524128bdfb15
web202606 <tabesa2255@gmail.com>
2026-08-16 01:12:46 +0900

    “20260815-画面等作成”

--- app/app/Http/Controllers/DisplayController.php
+++ app/app/Http/Controllers/DisplayController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use App\Html;
 use App\Css;
 use App\Javascript;
@@ -17,6 +19,7 @@ class DisplayController extends Controller
 {
     //
     public function top(){
+    //TOP画面表示
         return view('top');
         //return view('parts/header');
         
@@ -57,13 +60,15 @@ class DisplayController extends Controller
     //html登録スキル検索結果画面表示
     public function htmllist(Request $request){
         // 空を用意することで(検索結果0件で)値がなくてもエラーにならない
+        $html = new Html;
         $htmls = [];
         // htmlテーブルに値がある場合
         /*if(Auth::user()->html()->exists()) {
             $htmls = Auth::user()->html()->where('del_flg', 0)->get()->toArray();
         }*/
-        if(Html()->exists()) {
-            $htmls = Html()->where('del_flg', 0)->get()->toArray();
+        
+        if($html->exists()) {
+            $htmls = $html->where('del_flg', 0)->get()->toArray();
         }
 
         // ---------- 日付検索による支出一覧表示 ----------
@@ -79,30 +84,87 @@ class DisplayController extends Controller
             $until = $request['until'];
             
             //$htmls = Auth::user()->html()->wherebetween('date', [$from, $until])->where('del_flg', 0)->get()->toArray();
-            $htmls = Html()->wherebetween('date', [$from, $until])->where('del_flg', 0)->get()->toArray();
+            $htmls = $html->wherebetween('date', [$from, $until])->where('del_flg', 0)->get()->toArray();
             
             // from 選択された場合
         } elseif ($request['from']) {
             $from = $request['from'];
             //$htmls = Auth::user()->html()->where('date', '>=', $from )->where('del_flg', 0)->get()->toArray();
-            $htmls = Html()->where('date', '>=', $from )->where('del_flg', 0)->get()->toArray();
+            $htmls = $html->where('date', '>=', $from )->where('del_flg', 0)->get()->toArray();
            
             
             // until 選択された場合
         } elseif ($request['until']) {
             $until = $request['until'];
             //$htmls = Auth::user()->html()->where('date', '<=', $until )->where('del_flg', 0)->get()->toArray();
-            $htmls = Html()->where('date', '<=', $until )->where('del_flg', 0)->get()->toArray();
+            $htmls = $html->where('date', '<=', $until )->where('del_flg', 0)->get()->toArray();
            
            
 
             // from until 両方選択されなかった場合
         } else {
             //$htmls = Auth::user()->html()->where('del_flg', 0)->get()->toArray();
-            $htmls = Html()->where('del_flg', 0)->get()->toArray();
+            $htmls = $html->where('del_flg', 0)->get()->toArray();
         }
         return view('content/html/html_list', compact('htmls','until','from'));
     }    
+    public function htmlskill(Html $html){
+        //$html = new Html;
+        //$result = $html->find($id);
+        $pointmax =30;
+        $pointmin =10;
+        $pointhigh = 3;
+        $htmlcomment =[];
+        $html_point1 = $html->html_structure;
+        $html_point2 = $html->html_property;
+        $html_point3 = $html->html_posision;
+        $html_point4 = $html->html_link;
+        $html_point5 = $html->html_form;
+        $html_point6 = $html->html_table;
+        $html_point7 = $html->html_path;
+        $html_point8 = $html->html_element;
+        $html_point9 = $html->html_tool;
+        $html_point10 = $html->html_web;
+        $html_sum = $html_point1 + $html_point2 + $html_point3 + $html_point4 + $html_point5 + $html_point6 + $html_point7 + $html_point8 + $html_point9 + $html_point10;
 
+        //理解が完璧でない項目のコメントを取得  
+        if($html_point1 !== $pointhigh){
+            $htmlcomment[0] = '・Webサイトの仕組み';
+        } 
+        if($html_point2 !== $pointhigh){
+            $htmlcomment[1] = '・HTMLの属性';
+        } 
+        if($html_point3 !== $pointhigh){
+            $htmlcomment[2] = '・要素の配置ルール';
+        }
+        if($html_point4 !== $pointhigh){
+            $htmlcomment[3] = '・リンク';
+        } 
+        if($html_point5 !== $pointhigh){
+            $htmlcomment[4] = '・フォーム';
+        } 
+        if($html_point6 !== $pointhigh){
+            $htmlcomment[5] = '・テーブル';
+        } 
+        if($html_point7 !== $pointhigh){
+            $htmlcomment[6] = '・絶対パスと相対パス';
+        } 
+        if($html_point8 !== $pointhigh){
+            $htmlcomment[7] = '・ブロック要素とインライン要素';
+        } 
+        if($html_point9 !== $pointhigh){
+            $htmlcomment[8] = '・検証ツール';
+        }
+        if($html_point10 !== $pointhigh){
+            $htmlcomment[9] = '・WEBページ自作';
+        } 
+        //空の配列を詰める
+        $htmlcommentlist = array_filter($htmlcomment); 
+        return view('content/html/html_skill', [
+            'point' => $html_sum,
+            'comments' => $htmlcommentlist
+        ]);
+
+    }
     
 }
--- app/resources/views/content/html/html_list.blade.php
+++ app/resources/views/content/html/html_list.blade.php
@@ -1,6 +1,6 @@
 @extends('base')
 @section('head')
-<title>{{ config('app.name', 'HTMLスキル検索結果画面') }}</title>
+<title>{{ config('app.name', 'HTMLスキル登録履歴画面') }}</title>
 @endsection
 @section('main')
     <main>
@@ -9,36 +9,38 @@
                     <div class="row gx-5 justify-content-center">
                         <div class="col-md-8">
                             <div class="text-center my-5">
-                                <h1 class="display-5 fw-bolder text-white mb-3  text-nowrap">HTMLスキル検索結果画面</h1>
+                                <h1 class="display-5 fw-bolder text-white mb-3  text-nowrap">HTMLスキル登録履歴</h1>
                                 <p class="lead text-white-50 mb-1 text-nowrap">下記からスキル詳細を指定してください</p>
                             </div>
                         </div>
                     </div>
             </div>
         </div>
-        <div class="row justify-content-around">
+        <div class="row justify-content-around mt-5">
             <div class="col-md-4">
                 <div class="card">
-                    <div class="card-header">
-                        <div class='text-center'>登録リスト</div>
+                    <div class="card-header bg-primary">
+                        <div class='text-center'>登録履歴リスト</div>
                     </div>
                     <div class="card-body">
                         <div class="card-body">
                             <table class='table'>
-                                <thead>
+                                <thead class='bg-success'>
                                     <tr>
-                                        <th scope='col'>スキル詳細</th>
+                                        <th scope='col'>ID</th>
                                         <th scope='col'>日付</th>
+                                        <th scope='col'>スキル詳細</th>                                       
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <!-- ここに収入を表示する -->
                                         @foreach($htmls as $html)
                                         <tr>
+                                            <th scope='col'>{{$html['id']}}</th>
+                                            <th scope='col'>{{$html['date']}}</th>
                                             <th scope='col'>
-                                                <a href="{{ route('html_skill', ['html' => $html['id']]) }}">#</a>
-                                            </th>
-                                            <th scope='col'>{{$html['date']}}</th>      
+                                                <a href="{{ route('html_skill', ['html' => $html['id']]) }}">スキル</a>
+                                            </th>                                                  
                                         </tr>
                                     @endforeach
                                 </tbody>
