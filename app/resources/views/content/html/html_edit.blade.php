86b4a2d54c86bae23c112ea4b1290ac2aaeb2262
web202606 <tabesa2255@gmail.com>
2026-08-29 04:29:15 +0900

    “20260828-画面等作成”

--- app/resources/views/content/html/html_edit.blade.php
+++ app/resources/views/content/html/html_edit.blade.php
@@ -219,12 +219,12 @@
                         <textarea class='form-control' name='comment'>{{ old('comment') }}</textarea>
                     @endif
                 <div class='row justify-content-center'>
-                    <button type='submit' class='btn btn-primary w-25 mt-3'>更新</button>
+                    <button type='submit' class='btn btn-primary w-50 mt-3'>更新</button>
                 </div> 
             </form>
         </div>    
         <div class="fixed-bottom">
-            <a  class="col-md-1 w-100" href="{{ url()->previous() }}"><button  type="button" class="btn btn-primary btn-lg" role="button">戻る</button></a>
+            <a  class="col-md-1 w-100" href="{{ route('html_list') }}"><button  type="button" class="btn btn-primary btn-lg" role="button">戻る</button></a>
         </div>       
     </main>
 @endsection
